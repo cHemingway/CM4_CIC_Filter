33241e26c9c4690d59f95460201a39d9f911ba38
Christopher Hemingway <chris.hemingway@gmail.com>
2014-03-22 18:10:00 +0000

    Changed state variable to be 32 bit

--- cic.h
+++ cic.h
@@ -11,16 +11,17 @@
 
 #ifndef CMSIS_DSP
 typedef int16_t q15_t;
+typedef int32_t q32_t;
 #endif
 
 struct cic_decimate_instance_q15 {
 	uint16_t M;			/* Decimation Factor */
 	uint8_t  N; 		/* Order */
-	q15_t	 *pState;	/* State variable array */
+	q32_t	 *pState;	/* State variable array */
 };
 
 
-int cic_decimate_init_q15(cic_decimate_instance_q15 *S, uint16_t M, uint8_t N, q15_t *pState, uint32_t blockSize);
+int cic_decimate_init_q15(cic_decimate_instance_q15 *S, uint16_t M, uint8_t N, q32_t *pState, uint32_t blockSize);
 							 
 void cic_decimate_q15(const cic_decimate_instance_q15 *, q15_t *pSrc, q15_t *pDst, uint32_t blockSize);
 
