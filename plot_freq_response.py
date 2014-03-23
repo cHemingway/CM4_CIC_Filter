da119f534e888a0ac446e7b25cac5f64df3fe7ee
Chris Hemingway <chris.hemingway@gmail.com>
2014-03-23 23:26:53 +0000

    Update plot_freq_response.py to use proper axis

--- plot_freq_response.py
+++ plot_freq_response.py
@@ -8,12 +8,14 @@ import numpy as np
 M = 1000
 length = 10000
 n_steps = 400.0
-A = 1000
+A = 2000
+A_rms = A / np.sqrt(2.0)
 
 gains = []
+freqs = np.linspace(0,0.5,n_steps)
 
 #For each frequency
-for freq in np.linspace(0,0.5,n_steps):
+for freq in freqs:
     #Generate input data
     with open("sin.txt","w") as f:
         for i in xrange(0, length):
@@ -23,14 +25,17 @@ for freq in np.linspace(0,0.5,n_steps):
     subprocess.call("./test_cic %s %d" % ("sin.txt",M), shell=True)
 
     #Read the output data
-    dataout = []
+    dataout = np.zeros((length,1))
+    i = 0
     with open("out.txt") as f:
         for line in f:
-            dataout.append(int(line))
+            dataout[i] = int(line)
+            i+=1
 
-    dataout = np.array(dataout)
-    gains.append(np.sqrt(np.mean(dataout**2))) #Append RMS
+    gains.append(np.sqrt(np.mean(dataout**2))/A_rms) #Append RMS
 
-plt.semilogy(gains)
+plt.semilogy(freqs, gains)
+plt.ylabel("Gain (dB)")
+plt.xlabel("Normalised Frequency")
 plt.savefig("freq_resp.png")
 plt.show()
