fa7d75a405d858b423fb5cb950a8a9b0acd37a29
Christopher Hemingway <chris.hemingway@gmail.com>
2014-03-31 20:34:37 +0100

    Add initial implementation of cic_decimate_q32 (not working)

--- cic.c
+++ cic.c
@@ -62,6 +62,29 @@ int cic_decimate_init_q32(cic_decimate_instance_q32 *S, uint16_t M, uint8_t N, u
 
 void cic_decimate_q32(const cic_decimate_instance_q32 *S, q15_t *pSrc, q15_t *pDst, uint32_t blockSize)
 {
+	int i, j = 0;
+	q32_t *pInt =  	S->pState1; 	/*State after integrator */
+	q32_t *pComb = 	S->pState2;		/*State after decimator */
+	uint16_t M = 	S->M; 			/*Decimation Factor */
+	uint8_t	 R = 	S->R;			/*Differential delay scale in comb */
+	uint16_t outsize = blockSize/M;	/* Size of output */
 
+	/* INTEGRATE */
+	pInt[0] = pInt[blockSize-1] + pSrc[0]; 	/* Wrap around from last block */
+	for (i=1; i<blockSize; i++) {
+		pInt[i] = pInt[i-1] + pSrc[i];
+	}
 
+	/* DECIMATE */
+	for (i=0, j=0; i<blockSize; i=i+M, j++)
+	{
+		pComb[j] = pInt[i];
+	}
+
+	/* COMB */
+	pDst[0] = pComb[outsize] - pComb[outsize-R];
+	for (j = R; j<outsize; j++)
+	{
+		pDst[j] = pComb[j] - pComb[j-R];
+	}
 }
